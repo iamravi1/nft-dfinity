399e97c3f7b10970d9c08ad9633d815ec85bf3bb
iamravi1 <ravitejapeetani1@gmail.com>
2022-08-07 13:46:42 +0530

    first commit

---
+++ src/declarations/nft/nft.did.d.ts
@@ -0,0 +1,9 @@
+import type { Principal } from '@dfinity/principal';
+export interface NFT {
+  'getAsset' : () => Promise<Array<number>>,
+  'getCanisterId' : () => Promise<Principal>,
+  'getName' : () => Promise<string>,
+  'getOwner' : () => Promise<Principal>,
+  'transferOwnerShip' : (arg_0: Principal) => Promise<string>,
+}
+export interface _SERVICE extends NFT {}
